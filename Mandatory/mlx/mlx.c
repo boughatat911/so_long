1a2523bdacd41cf9f495c4f06e7ec9fbc25fdb5a
Naoufal Bougrine <nbougrin@e1r8p5.1337.ma>
2025-02-07 16:35:09 +0100

    up_mlx_window

--- Mandatory/mlx/mlx.c
+++ Mandatory/mlx/mlx.c
@@ -6,117 +6,90 @@
 /*   By: nbougrin <nbougrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 10:37:00 by nbougrin          #+#    #+#             */
-/*   Updated: 2025/02/05 20:31:38 by nbougrin         ###   ########.fr       */
+/*   Updated: 2025/02/07 16:30:05 by nbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 #include "/home/nbougrin/boughatat/so_long/Mandatory/so_long.h"
-// typedef struct s_textures {
-// 	void	*wall;
-// 	void	*floor;
-// 	void	*player;
-// 	void	*collectible;
-// 	void	*exit;
-// } t_textures;
-
-void	*initialize_mlx(void)
-{
-	void	*mlx;
-
-	mlx = mlx_init();
-	if (!mlx)
-		printf("Error: Failed to initialize MLX\n");
-	return (mlx);
-}
 
 void	*create_window(void *mlx, char **map, int *win_width, int *win_height)
 {
 	int	map_width;
 	int	map_height;
 
-	map_width = ft_strlen(map[0]);  // Use strlen for calculating map width
+	map_width = ft_strlen(map[0]);
 	map_height = 0;
-	while (map[map_height])  // Count rows of the map
+	while (map[map_height])
 		map_height++;
-	
 	*win_width = map_width * TILE_SIZE;
 	*win_height = map_height * TILE_SIZE;
-	return (mlx_new_window(mlx, *win_width, *win_height, "so_long"));
+	return (mlx_new_window(mlx, *win_width, *win_height, "GTA"));
 }
 
-void	load_textures(void *mlx, t_textures *textures)
+void	load_textures(void *mlx, t_game *game)
 {
 	int	w;
 	int	h;
 
-	textures->wall = mlx_xpm_file_to_image(mlx, "wall.xpm", &w, &h);
-	textures->floor = mlx_xpm_file_to_image(mlx, "floor.xpm", &w, &h);
-	textures->player = mlx_xpm_file_to_image(mlx, "player.xpm", &w, &h);
-	textures->collectible = mlx_xpm_file_to_image(mlx, "collectible.xpm", &w, &h);
-	textures->exit = mlx_xpm_file_to_image(mlx, "exit.xpm", &w, &h);
+	w = 0;
+	h = 0;
+	game->textures.wall = mlx_xpm_file_to_image(mlx, "wall.xpm", &w, &h);
+	game->textures.floor = mlx_xpm_file_to_image(mlx, "floor.xpm", &w, &h);
+	game->textures.player = mlx_xpm_file_to_image(mlx, "player.xpm", &w, &h);
+	game->textures.collectible = mlx_xpm_file_to_image(mlx,
+			"collectible.xpm", &w, &h);
+	game->textures.exit = mlx_xpm_file_to_image(mlx, "exit.xpm", &w, &h);
 }
 
-void	render_tile(void *mlx, void *win, void *texture, int x, int y)
+void	render_tile(t_game *game, void *texture)
 {
 	if (texture)
-		mlx_put_image_to_window(mlx, win, texture, 
-		                        x * TILE_SIZE, y * TILE_SIZE);
+		mlx_put_image_to_window(game->mlx,
+			game->win, texture, game->x * TILE_SIZE, game->y * TILE_SIZE);
 }
 
-void	render_map(void *mlx, void *win, char **map, t_textures *textures)
+void	render_map(t_game *game, char **map)
 {
-	int	y;
-	int	x;
-
-	y = 0;
-	while (map[y])
+	game->y = 0;
+	while (map[game->y])
 	{
-		x = 0;
-		while (map[y][x])
+		game->x = 0;
+		while (map[game->y][game->x])
 		{
-			if (map[y][x] == '1')
-				render_tile(mlx, win, textures->wall, x, y);
-			else if (map[y][x] == '0')
-				render_tile(mlx, win, textures->floor, x, y);
-			else if (map[y][x] == 'P')
-				render_tile(mlx, win, textures->player, x, y);
-			else if (map[y][x] == 'C')
-				render_tile(mlx, win, textures->collectible, x, y);
-			else if (map[y][x] == 'E')
-				render_tile(mlx, win, textures->exit, x, y);
-			x++;
+			if (map[game->y][game->x] == '1')
+				render_tile(game, game->textures.wall);
+			else if (map[game->y][game->x] == '0')
+				render_tile(game, game->textures.floor);
+			else if (map[game->y][game->x] == 'P')
+				render_tile(game, game->textures.player);
+			else if (map[game->y][game->x] == 'C')
+				render_tile(game, game->textures.collectible);
+			else if (map[game->y][game->x] == 'E')
+				render_tile(game, game->textures.exit);
+			game->x++;
 		}
-		y++;
+		game->y++;
 	}
-}
-
-void	free_resources(void *mlx, void *win, t_textures *textures)
-{
-	mlx_destroy_image(mlx, textures->wall);
-	mlx_destroy_image(mlx, textures->floor);
-	mlx_destroy_image(mlx, textures->player);
-	mlx_destroy_image(mlx, textures->collectible);
-	mlx_destroy_image(mlx, textures->exit);
-	mlx_destroy_window(mlx, win);
 }
 
 void	mlx_map(char **map)
 {
-	void	*mlx;
-	void	*win;
+	t_game	game;
 	int		win_width;
 	int		win_height;
-	t_textures	textures;
 
-	mlx = initialize_mlx();
-	if (!mlx)
-		return;
-	win = create_window(mlx, map, &win_width, &win_height);
-	if (!win)
-		return(free(mlx));
-	load_textures(mlx, &textures);
-	render_map(mlx, win, map, &textures);
-	mlx_loop(mlx);
-	free_resources(mlx, win, &textures);
+	game.mlx = mlx_init();
+	if (!game.mlx)
+		return ;
+	game.win = create_window(game.mlx, map, &win_width, &win_height);
+	if (!game.win)
+	{
+		free(game.mlx);
+		return ;
+	}
+	load_textures(game.mlx, &game);
+	render_map(&game, map);
+	mlx_loop(game.mlx);
+	mlx_destroy_window(game.mlx, game.win);
 }
