274ab80f37bbd6a7df27fe15c51fead2a1e57244
Naoufal Bougrine <nbougrin@e1r7p14.1337.ma>
2025-02-13 13:45:43 +0100

    remove

--- tmp.c
+++
@@ -1,8 +0,0 @@
-// void	free_resources(t_game *game)
-// {
-// 	// mlx_destroy_image(game->mlx, game->textures.wall);
-// 	// mlx_destroy_image(game->mlx, game->textures.floor);
-// 	// mlx_destroy_image(game->mlx, game->textures.player);
-// 	// mlx_destroy_image(game->mlx, game->textures.collectible);
-// 	// mlx_destroy_image(game->mlx, game->textures.exit);
-// }
